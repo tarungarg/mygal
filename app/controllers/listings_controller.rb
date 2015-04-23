5892085b1d3c1d63aa13ce21ffed996ad6c48071
mayanovarini@gmail.com <mayanovarini@gmail.com>
2015-04-23 16:45:19 -0700

    set user permission

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,7 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
 
   # GET /listings
   # GET /listings.json
